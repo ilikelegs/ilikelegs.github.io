aac737e9bbda45812b1279a3608723faef61bb63
ilikelegs <bld163@126.com>
2019-09-13 17:16:09 +0800

    name

--- photo.py
+++ photo.py
@@ -1,7 +1,9 @@
 from PIL import Image
 import os
+import uuid
+import json
 
-def compress_image(infile, outfile='', mb=130, step=10, quality=80):
+def compress_image(infile, outfile='', mb=140, step=10, quality=80):
     """不改变图片尺寸压缩到指定大小
     :param infile: 压缩源文件
     :param outfile: 压缩文件保存地址
@@ -36,12 +38,30 @@ def get_outfile(infile, outfile):
     return outfile
 
 if __name__ == '__main__':
-    # 遍历重命名
+    # json数组
+    result = []
+    # 遍历文件数量生成名字
     path = os.walk("./photos")
-    count = 1
     for root, dirs, files in path:
         for f in files: 
-            os.rename(os.path.join(root, f),os.path.join(root,str(count)+".jpg"))
+            fname = uuid.uuid5(uuid.NAMESPACE_DNS, f)
+            temp_dict = {}
+            temp_dict['title'] = ""
+            temp_dict['url'] = "photos_compress/" + str(fname) + ".jpg"
+            temp_dict['href'] = "photos/" + str(fname) + ".jpg"
+            temp_dict['content'] = ""
+            temp_dict['name'] = str(fname) + ".jpg"
+            result.append(temp_dict)
+    # 写入json文件
+    with open("./data/photos.json", 'w') as f:
+        json.dump(result, f)
+
+    # 遍历重命名
+    path = os.walk("./photos")
+    count = 0
+    for root, dirs, files in path:
+        for f in files: 
+            os.rename(os.path.join(root, f),os.path.join(root,result[count]["name"]))
             count+=1
     # 遍历删除压缩图片
     path = os.walk("./photos_compress")
