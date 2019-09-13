2ec431b5e50e1a3eca0a53ce98ff99d8bac33e61
ilikelegs <bld163@126.com>
2019-09-13 17:35:58 +0800

    compressimage

--- photo.py
+++ photo.py
@@ -3,6 +3,33 @@ import os
 import uuid
 import json
 
+#图片压缩批处理
+def compressImage(srcPath,dstPath):
+    for filename in os.listdir(srcPath):
+        #如果不存在目的目录则创建一个，保持层级结构
+        if not os.path.exists(dstPath):
+                os.makedirs(dstPath)
+ 
+        #拼接完整的文件或文件夹路径
+        srcFile=os.path.join(srcPath,filename)
+        dstFile=os.path.join(dstPath,filename)
+ 
+        # 如果是文件就处理
+        if os.path.isfile(srcFile):
+            try:
+                #打开原图片缩小后保存，可以用if srcFile.endswith(".jpg")或者split，splitext等函数等针对特定文件压缩
+                sImg=Image.open(srcFile)
+                w,h=sImg.size
+                dImg=sImg.resize((int(w/2),int(h/2)),Image.ANTIALIAS)  #设置压缩尺寸和选项，注意尺寸要用括号
+                dImg.save(dstFile) #也可以用srcFile原路径保存,或者更改后缀保存，save这个函数后面可以加压缩编码选项JPEG之类的
+                print (dstFile+" 成功！")
+            except Exception:
+                print(dstFile+"失败！！！！！！！！！！！！！！！！！！！！！！！！！！！！")
+ 
+        # 如果是文件夹就递归
+        if os.path.isdir(srcFile):
+            compressImage(srcFile, dstFile)
+
 def compress_image(infile, outfile='', mb=140, step=10, quality=80):
     """不改变图片尺寸压缩到指定大小
     :param infile: 压缩源文件
@@ -69,7 +96,8 @@ if __name__ == '__main__':
         for f in files:
             os.remove(os.path.join(root, f))
     # 遍历压缩图片
-    path = os.walk("./photos")
-    for root, dirs, files in path:
-        for f in files: 
-            compress_image(os.path.join(root, f),os.path.join("./photos_compress/", f))
+    compressImage("./photos","./photos_compress")
+    # path = os.walk("./photos")
+    # for root, dirs, files in path:
+    #     for f in files: 
+    #         compress_image(os.path.join(root, f),os.path.join("./photos_compress/", f))
